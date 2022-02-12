9fb27e00518dc86d0680f85070f5298adecdeffe
Aaran Xu <aaranxu@outlook.com>
2022-02-13 04:10:20 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,3 +19,20 @@ touch .
 git add -A .
 git commit -m "rebuild pages at ${rev}"
 git push -q upstream HEAD:gh-pages > /dev/null 2>&1
+
+
+cd ../english/book
+
+git init
+git config user.name "Aaran Xu"
+git config user.email "aaranxu@outlook.com"
+
+git remote add upstream "git@github.com:rust-lang-cn/book-cn.git"
+git fetch upstream
+git reset upstream/gh-pages-en
+
+touch .
+
+git add -A .
+git commit -m "rebuild English pages at ${rev}"
+git push -q upstream HEAD:gh-pages-en > /dev/null 2>&1
