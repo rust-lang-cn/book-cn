2b88471138132fc86c4d48cb539b7525f8393d8e
YangFong <70502828+YangFong@users.noreply.github.com>
2022-03-07 19:18:42 +0800

    docs:  add missing notes

--- listings/ch04-understanding-ownership/no-listing-15-dangling-reference-annotated/src/main.rs
+++ listings/ch04-understanding-ownership/no-listing-15-dangling-reference-annotated/src/main.rs
@@ -3,11 +3,11 @@ fn main() {
 }
 
 // ANCHOR: here
-fn dangle() -> &String { // / dangle 返回一个字符串的引用
+fn dangle() -> &String { // dangle 返回一个字符串的引用
 
     let s = String::from("hello"); // s 是一个新字符串
 
-    &s // // 返回字符串 s 的引用
+    &s // 返回字符串 s 的引用
 } // 这里 s 离开作用域并被丢弃。其内存被释放。
   // 危险！
 // ANCHOR_END: here
